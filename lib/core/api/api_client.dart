415ecb205b78b31b35285c992e3ffa16221ad904
RethyLS <loeungsounrethy@gmail.com>
2026-03-27 14:17:31 +0700

    Fix Release APK connection issues by adding missing INTERNET permission and improving the dynamic API URL resolver

--- lib/core/api/api_client.dart
+++ lib/core/api/api_client.dart
@@ -3,7 +3,6 @@ import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:device_info_plus/device_info_plus.dart';
 import 'package:hybrid_digital_docs_assignment_frontend/core/utils/image_utils.dart';
 
 final sharedPrefsProvider = FutureProvider<SharedPreferences>((ref) async {
@@ -28,20 +27,21 @@ final dioProvider = Provider<Dio>((ref) {
         if (globalResolvedBaseUrl == null) {
           if (!kIsWeb && Platform.isAndroid) {
             try {
-              final androidInfo = await DeviceInfoPlugin().androidInfo;
-              if (androidInfo.isPhysicalDevice) {
-                // Physical device connected via USB (adb reverse)
-                globalResolvedBaseUrl = 'http://127.0.0.1:8000/api';
-              } else {
-                // Android Emulator mapping to Host PC
-                globalResolvedBaseUrl = 'http://10.0.2.2:8000/api';
-              }
-            } catch (e) {
-              // Failsafe
+              // Create a temporary Dio instance to "ping" the emulator host IP
+              final pingDio = Dio(BaseOptions(
+                connectTimeout: const Duration(milliseconds: 1500),
+              ));
+              
+              // If we are on an emulator, 10.0.2.2 will respond or at least not "Refuse Connection" instantly.
+              // We just check if the host is reachable.
+              await pingDio.get('http://10.0.2.2:8000/api/login');
               globalResolvedBaseUrl = 'http://10.0.2.2:8000/api';
+            } catch (e) {
+              // If 10.0.2.2 fails/times out, we are likely on a physical device using 'adb reverse'
+              globalResolvedBaseUrl = 'http://127.0.0.1:8000/api';
             }
           } else {
-            // iOS Simulators, Web, Desktop
+            // iOS, Web, and Desktop always use 127.0.0.1
             globalResolvedBaseUrl = 'http://127.0.0.1:8000/api';
           }
         }
